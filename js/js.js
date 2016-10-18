29f83b45b54c7537a22a6ff9e71cd5538acfd0cc
Ellie Angell and Stephan Khoury <stephan@>
2016-10-18 10:52:33 -0700

    add buttons

---
+++ js/js.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+  });
+});
