d3a586ca7930dccf9f2cc6d33641808d7e0b90e2
Ellie Angell and Stephan Khoury <ellie@>
2016-10-18 11:47:31 -0700

    add highlighted box

--- js/js.js
+++ js/js.js
@@ -13,4 +13,9 @@ $(document).ready(function() {
     $("body").removeClass();
     $("body").addClass("white-background");
   });
+
+  $(".clickable").click(function() {
+
+    $(this).toggleClass("box");
+  });
 });
